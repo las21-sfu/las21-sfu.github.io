7560b3c1e327d141c52d2fea74fd072194d6193c
Laraine Sim <las21@sfu.ca>
2026-02-02 00:25:26 -0800

    Added simple JS to main page

---
+++ main.js
@@ -0,0 +1,16 @@
+const revealElements = document.querySelectorAll(".reveal");
+
+const revealObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.15
+  }
+);
+
+revealElements.forEach((el) => revealObserver.observe(el));
