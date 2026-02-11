f375a6c5c0b1abb68363eac03767d299afae4641
Laraine Sim <las21@sfu.ca>
2026-02-10 22:16:10 -0800

    Beginning to set up Assignment 3

--- main.js
+++ main.js
@@ -14,3 +14,31 @@ const revealObserver = new IntersectionObserver(
 );
 
 revealElements.forEach((el) => revealObserver.observe(el));
+
+const spec = {
+  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+  "description": "Video game sales visualization",
+  "data": {
+    "url": "data/videogames_wide.csv",
+    "format": {
+      "type": "csv"
+    }
+  },
+  "mark": "bar",
+  "encoding": {
+    "x": {
+      "field": "Platform",
+      "type": "nominal",
+      "title": "Platform"
+    },
+    "y": {
+      "aggregate": "sum",
+      "field": "Global_Sales",
+      "type": "quantitative",
+      "title": "Global Sales (millions)"
+    }
+  }
+};
+
+vegaEmbed("#vis", spec)
+  .catch(console.error);
